376e082b1d65ba9470d8eece4370b70af539efc8
Nathan Pabst <nathan.pabst@gmail.com>
2019-03-16 12:05:42 -0500

    using the null-conditional operator with an array.

--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -10,16 +10,19 @@ namespace GameConsole
     {
         static void Main(string[] args)
         {
-            PlayerCharacter player = new PlayerCharacter();
-            player.DaysSinceLastLogin = 42;
+            //PlayerCharacter[] players = new PlayerCharacter[3]
+            //    {
+            //        new PlayerCharacter { Name = "Sarah"},
+            //        new PlayerCharacter(), //Name = null
+            //        null // PlayerCharacter = null
+            //    };
 
-            int days = player?.DaysSinceLastLogin ?? -1;
+            PlayerCharacter[] players = null;
 
-            
-
-            //int days = player.DaysSinceLastLogin.Value;
-
-            Console.WriteLine(days);
+            //using the null-conditional operator to reference elements in an array
+            string p1 = players?[0]?.Name;
+            string p2 = players?[1]?.Name;
+            string p3 = players?[2]?.Name;
 
             Console.ReadLine();
         }
